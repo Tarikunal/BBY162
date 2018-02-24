1b2f3a0fd66d36241d237dbaa470807e7a42a36b
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-24 20:37:38 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,8 @@
+_auther_='Tarık Ünal'
+#Uygulama0I
+
+metin_yarısı = "Offices parties lasting outward nothing age few resolve. Impression to discretion understood to we interested he excellence."
+
+#print(len(metin_yarısı)) #^^==124==^^
+
+print(metin_yarısı[0:62])
