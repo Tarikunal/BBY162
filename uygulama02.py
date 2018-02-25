0eaa7b4eae7ccfd9c20819e6c0d772de2518d7fe
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-25 13:31:36 +0300

    Add files via upload

---
+++ uygulama02.py
@@ -0,0 +1,11 @@
+_auther_='Tarık Ünal'
+#Saniye cinsinden verilen veriyi yıl, ay, gün, saat, dakika ve saniyeye dönüştüren formül..
+girdi = int(input("Saniye cinsinden bir değer giriniz...:"))
+yil = girdi / 31557600
+ay = (girdi % 31557600) / 2629800
+gun = ((girdi % 31557600) % 2629800) / 86400
+saat = (((girdi% 31557600) % 2629800) % 86400) / 3600
+dakika = ((((girdi % 31557600) % 2629800) % 86400)% 3600) / 60
+saniye = (((((girdi % 31557600) % 2629800) % 86400) % 3600) % 60)
+
+print(int(yil) , "Yıl" , " " , int(ay) ,"Ay" , " " , int(gun) ,"Gün" , " " , int(saat) , "Saat" , " " , int(dakika)  ,"Dakika", " " , int(saniye) , "Saniye")
